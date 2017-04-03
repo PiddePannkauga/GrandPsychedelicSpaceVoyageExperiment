f8ed820f51a8acb67768878f03c16604f2b648c2
PiddePannkauga <petter.mansson1@gmail.com>
2017-04-03 14:13:19 +0200

    Spotify
    Kod som innehåller koppling till spotify

---
+++ Android/SoundDemo/app/src/main/java/com/gpsve/sounddemo/MainActivityGammal.java
@@ -0,0 +1,52 @@
+package com.gpsve.sounddemo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.media.MediaPlayer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import processing.android.PFragment;
+
+public class MainActivityGammal extends AppCompatActivity {
+    private VisualizerDemo demo;
+    private MediaPlayer player;
+    private int yo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        demo = new VisualizerDemo();
+        PFragment fragment = new PFragment();
+        fragment.setSketch(demo);
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment, fragment)
+                .commit();
+        init();
+    }
+
+    private void init()
+    {
+        //player = new MediaPlayer();
+        //player.setAudioSessionId(0);
+        //player.setLooping(true);
+        //player.start();
+
+//        Log.v("HEJ",""+(player.getAudioSessionId()));
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                yo);
+
+        demo.link();
+
+        // Start with just line renderer
+
+    }
+
+
+
+}
