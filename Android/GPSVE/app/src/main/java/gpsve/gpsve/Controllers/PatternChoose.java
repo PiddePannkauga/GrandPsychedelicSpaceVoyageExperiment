cf0797b94d2dc7ce7c7f67b5366ad920f27bc061
nilslindkvist <lindkvist.nils@gmail.com>
2017-05-12 16:30:47 +0200

    Nya nisse patturnos
    Rensat i mainrelease och lagt till PatternChoose och PatternNisse från
    nils mapp

--- Android/GPSVE/app/src/main/java/gpsve/gpsve/Controllers/PatternChoose.java
+++ Android/GPSVE/app/src/main/java/gpsve/gpsve/Controllers/PatternChoose.java
@@ -1,7 +1,7 @@
-package gpsve.gpsve.Controllers;
+package gpsve.gpsve;
 
-import gpsve.gpsve.Interface.PatternInterface;
 import processing.core.PApplet;
+import processing.core.PFont;
 
 /**
  * Created by Nils on 2017-04-24.
@@ -9,6 +9,9 @@ import processing.core.PApplet;
 
 public class PatternChoose implements PatternInterface {
     private PApplet parent;
+    private final int[] purpColor = new int[]{255, 0, 225};
+    private final int[] turqColor = new int[]{0, 255, 235};
+    private int colorCount = 0;
 
     public PatternChoose(PApplet parent) {
         this.parent = parent;
@@ -16,12 +19,23 @@ public class PatternChoose implements PatternInterface {
 
     @Override
     public void updatePattern(byte[] fft, byte[] wave) {
-
+        if (colorCount < 4) {
+            parent.fill(purpColor[0], purpColor[1], purpColor[2]);
+            colorCount++;
+        } else if (colorCount < 8) {
+            parent.fill(turqColor[0], turqColor[1], turqColor[2]);
+            colorCount++;
+        } else {
+            colorCount = 0;
+        }
     }
 
     @Override
     public void drawPattern() {
         parent.background(0);
+        parent.textSize(parent.width*(float)0.2);
+        parent.textAlign(parent.CENTER, parent.CENTER);
+        parent.text("Choose a\npattern", parent.width/2, parent.height/2);
     }
 
     @Override
--- Android/Nils/GPSVE/app/src/main/java/gpsve/gpsve/PatternNisse.java
+++ Android/Nils/GPSVE/app/src/main/java/gpsve/gpsve/PatternNisse.java
@@ -9,9 +9,9 @@ import processing.core.*;
 public class PatternNisse implements PatternInterface {
     private PApplet parent;
     private byte[] fft, wave;
-    private int r, g, b, x1, y1, x2, y2, constant, fftAmp, waveAmp;
-    private float x3, y3, radius, angle;
-    private final int SPEED_SLOW = 12, SPEED_FAST = 20;
+    private int fftAmp, waveAmp;
+    private float[] ellipse = new float[6], ellipseDecay = new float[6], ellipseColor = new float[6];
+    private float speed;
     private boolean okToDraw = true;
 
     public PatternNisse(PApplet parent) {
@@ -21,16 +21,9 @@ public class PatternNisse implements PatternInterface {
 
     public void setup() {
         parent.background(0);
-        r = 0;
-        g = 25;
-        b = 50;
-        x1 = parent.width/2;
-        x2 = x1;
-        x3 = 0;
-        y1 = parent.height/2;
-        y2 = y1;
-        y3 = 0;
-        constant = SPEED_SLOW;
+        for(int i = 0; i < ellipseColor.length; i++) {
+            ellipseColor[i] = 175;
+        }
     }
 
     public void calculateFftAmp() {
@@ -63,6 +56,135 @@ public class PatternNisse implements PatternInterface {
 //        System.out.println("waveAmp = " + waveAmp);
     }
 
+    public void calculateFftEllipse() {
+        for(int i = 0; i < ellipse.length; i++) {
+            ellipse[i] = Math.min(parent.width, parent.height)*(float)0.05;
+        }
+
+        int mod;
+        for(int i = 0; i < fft.length; i++) {
+            if(fft[i] > 0) {
+                mod = i % 6;
+                ellipse[mod] += fft[i]*1.1;
+            }
+        }
+    }
+
+    public float[] ellipseDecay(float[] newEllipse) {
+        for(int i = 0; i < newEllipse.length; i++) {
+            if (newEllipse[i] <= ellipseDecay[i]) {
+                newEllipse[i] = ellipseDecay[i] * (float)0.98;
+                if (ellipseColor[i] > 100) {
+                    ellipseColor[i] -= 5;
+                }
+            } else {
+                ellipseColor[i] = 175;
+//                System.out.println("Ej DeCaY");
+            }
+        }
+
+        ellipseDecay = newEllipse.clone();
+
+        return newEllipse;
+    }
+
+    public void drawGrid() {
+        if(parent.mousePressed){
+            speed += 0.2;
+        } else {
+            speed += 0.15;
+        }
+
+        parent.pushMatrix();
+        parent.translate(parent.width/2, 0);
+
+        parent.noStroke();
+        parent.fill(255,0,225,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.1, parent.width*(float)0.2, 15);
+        parent.fill(0,255,235,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.2, parent.width*(float)0.2, 15);
+        parent.fill(255,0,225,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.3, parent.width*(float)0.2, 15);
+        parent.fill(0,255,235,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.4, parent.width*(float)0.2, 15);
+        parent.fill(255,0,225,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.5, parent.width*(float)0.2, 15);
+        parent.fill(0,255,235,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.6, parent.width*(float)0.2, 15);
+        parent.fill(255,0,225,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.7, parent.width*(float)0.2, 15);
+        parent.fill(0,255,235,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.8, parent.width*(float)0.2, 15);
+        parent.fill(255,0,225,100);
+        parent.ellipse(cos(parent.width/2, speed), parent.height*(float)0.9, parent.width*(float)0.2, 15);
+
+        parent.popMatrix();
+
+        parent.pushMatrix();
+        parent.translate(0, parent.height/2);
+
+        parent.noStroke();
+        parent.fill(0,255,235,100);
+        parent.ellipse(parent.width*(float)0.1, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(255,0,225,100);
+        parent.ellipse(parent.width*(float)0.2, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(0,255,235,100);
+        parent.ellipse(parent.width*(float)0.3, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(255,0,225,100);
+        parent.ellipse(parent.width*(float)0.4, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(0,255,235,100);
+        parent.ellipse(parent.width*(float)0.5, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(255,0,225,100);
+        parent.ellipse(parent.width*(float)0.6, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(0,255,235,100);
+        parent.ellipse(parent.width*(float)0.7, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(255,0,225,100);
+        parent.ellipse(parent.width*(float)0.8, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(0,255,235,100);
+        parent.ellipse(parent.width*(float)0.9, sin(parent.height/2, speed), 15, parent.height*(float)0.2);
+        parent.fill(255,0,225,100);
+
+        parent.popMatrix();
+    }
+
+    public void drawLines() {
+        parent.pushMatrix();
+        parent.translate(parent.width/2, parent.height/2);
+
+        parent.stroke(200,200,200);
+        parent.strokeWeight(Math.min(parent.width, parent.height)*(float)0.02);
+        parent.line(-parent.width*(float)0.2, -parent.height*(float)0.3, parent.width*(float)0.2, -parent.height*(float)0.3);
+        parent.line(-parent.width*(float)0.2, -parent.height*(float)0.3, -parent.width*(float)0.3, 0);
+        parent.line(parent.width*(float)0.2, -parent.height*(float)0.3, parent.width*(float)0.3, 0);
+        parent.line(-parent.width*(float)0.3, 0, -parent.width*(float)0.2, parent.height*(float)0.3);
+        parent.line(parent.width*(float)0.3, 0, parent.width*(float)0.2, parent.height*(float)0.3);
+        parent.line(-parent.width*(float)0.2, parent.height*(float)0.3, parent.width*(float)0.2, parent.height*(float)0.3);
+        parent.noStroke();
+
+        parent.popMatrix();
+    }
+
+    public void drawCircles() {
+        parent.pushMatrix();
+        parent.translate(parent.width/2, parent.height/2);
+
+        parent.stroke(0);
+        parent.fill(ellipseColor[0]);
+        parent.ellipse(-parent.width*(float)0.2, -parent.height*(float)0.3, ellipse[0], ellipse[0]);
+        parent.fill(ellipseColor[1]);
+        parent.ellipse(parent.width*(float)0.2, -parent.height*(float)0.3, ellipse[1], ellipse[1]);
+        parent.fill(ellipseColor[2]);
+        parent.ellipse(-parent.width*(float)0.3, 0, ellipse[2], ellipse[2]);
+        parent.fill(ellipseColor[3]);
+        parent.ellipse(parent.width*(float)0.3, 0, ellipse[3], ellipse[3]);
+        parent.fill(ellipseColor[4]);
+        parent.ellipse(-parent.width*(float)0.2, parent.height*(float)0.3, ellipse[4], ellipse[4]);
+        parent.fill(ellipseColor[5]);
+        parent.ellipse(parent.width*(float)0.2, parent.height*(float)0.3, ellipse[5], ellipse[5]);
+
+        parent.popMatrix();
+    }
+
     @Override
     public void updatePattern(byte[] fft, byte[] wave) {
         this.fft = fft;
@@ -70,53 +192,18 @@ public class PatternNisse implements PatternInterface {
 
         calculateFftAmp();
         calculateWaveAmp();
+        calculateFftEllipse();
+        ellipse = ellipseDecay(ellipse);
     }
 
     @Override
     public void drawPattern() {
         okToDraw = false;
-
         parent.background(0,3);
-        if(parent.mousePressed){
-            constant = SPEED_FAST;
-        } else {
-            constant = SPEED_SLOW;
-        }
 
-        parent.fill(r,15,15);
-        parent.ellipse(x1, y1, fftAmp, fftAmp);
-        parent.fill(15,g,15);
-        parent.ellipse(x1, y2, fftAmp, fftAmp);
-        parent.fill(15,15,b);
-        parent.ellipse(x2, y1, fftAmp, fftAmp);
-        parent.fill(r,50,b);
-        parent.ellipse(x2, y2, fftAmp, fftAmp);
-        parent.fill(50,g,b);
-        parent.ellipse(x3+parent.width/2, y3+parent.height/2, 40, 40);
-
-        r = (r+2)%25+230;
-        g = (g+2)%25+230;
-        b = (b+2)%25+230;
-
-        x1 = (x1+constant)%parent.width;
-        y1 = (y1+constant)%parent.height;
-        x2 = (x2-constant);
-        y2 = (y2-constant);
-        if(x2<0)
-            x2 = x2+parent.width;
-        if(y2<0)
-            y2 = y2+parent.height;
-
-        angle += 0.05;
-        radius += 0.5;
-        x3 = radius * parent.cos(angle);
-        y3 = radius * parent.sin(angle);
-        if(y3+parent.height/2 > parent.height) {
-            x3 = 0;
-            y3 = 0;
-            angle = 0;
-            radius = 0;
-        }
+        drawLines();
+        drawGrid();
+        drawCircles();
 
         okToDraw = true;
     }
@@ -124,5 +211,15 @@ public class PatternNisse implements PatternInterface {
     @Override
     public boolean okToDraw() {
         return okToDraw;
+    }
+
+    public float sin(float size, float speed) {
+        float pos = size * parent.sin(speed);// + parent.width/2;
+        return pos;
+    }
+
+    public float cos(float size, float speed) {
+        float pos = size * parent.cos(speed);// + parent.width/2;
+        return pos;
     }
 }
