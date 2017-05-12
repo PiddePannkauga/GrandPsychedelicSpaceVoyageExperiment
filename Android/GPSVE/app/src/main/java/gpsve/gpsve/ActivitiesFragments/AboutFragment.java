cb47c08065ec7b52f5ef401946d67cce3e4bc152
PiddePannkauga <petter.mansson1@gmail.com>
2017-05-12 15:52:00 +0200

    Ny mainRelease

---
+++ Android/GPSVE/app/src/main/java/gpsve/gpsve/ActivitiesFragments/AboutFragment.java
@@ -0,0 +1,49 @@
+package gpsve.gpsve.ActivitiesFragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import gpsve.gpsve.R;
+
+/**
+ * Created by Petter "Pidde" Månsson on 2017-05-09.
+ */
+public class AboutFragment extends Fragment {
+
+    public static ImageButton aboutButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.aboutfragment, container, false);
+        view.setBackgroundColor(Color.WHITE);
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        aboutButton = (ImageButton)getActivity().findViewById(R.id.aboutbutton);
+
+
+        aboutButton.setEnabled(false);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        aboutButton.setEnabled(true);
+
+
+    }
+}
---
+++ Android/GPSVE/app/src/main/java/gpsve/gpsve/Controllers/PatternChoose.java
@@ -0,0 +1,31 @@
+package gpsve.gpsve.Controllers;
+
+import gpsve.gpsve.Interface.PatternInterface;
+import processing.core.PApplet;
+
+/**
+ * Created by Nils on 2017-04-24.
+ */
+
+public class PatternChoose implements PatternInterface {
+    private PApplet parent;
+
+    public PatternChoose(PApplet parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void updatePattern(byte[] fft, byte[] wave) {
+
+    }
+
+    @Override
+    public void drawPattern() {
+        parent.background(0);
+    }
+
+    @Override
+    public boolean okToDraw() {
+        return true;
+    }
+}
